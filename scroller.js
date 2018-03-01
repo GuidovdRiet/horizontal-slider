9da9a7bb60ec74ac5841842b3ecc31b99a2008b8
Guido van de Riet <guidovdriet@gmail.com>
2018-03-01 14:50:26 +0100

    set navigation boundary for left side

--- scroller.js
+++ scroller.js
@@ -8,6 +8,7 @@ const scrollerIndicator = document.querySelector(
 );
 
 let scrollTotal = 0;
+let scrollerIndicatorPosition = 0;
 
 const moveSlider = e => {
   const scrollerWidth = -totalContentWidth(listItems);
@@ -21,20 +22,20 @@ const moveSlider = e => {
   scrollLimitChecker(scrollerWidth, e);
 };
 
-// Width van de slider / 100 * 10%; Is 10% van de slider width;
-// Als de sliderWidth 10% is, is dit 100% van de totaal mogelijke slider width
-// De speed moet dan ook 10% zijn van de totale snelheid waarop de slider beweegt 
-
 const scrollLimitChecker = (scrollerWidth, e) => {
-  // Hoeveel procent is de scroller van het geheel?
   const scrollerNavWidthPercent = scrollerWidth / scrollerNav.offsetWidth;
+  scrollerIndicatorPosition = scrollTotal / scrollerNavWidthPercent - scrollerIndicator.offsetWidth;
   if (scrollTotal >= scrollerWidth) {
     contentScroller.style.transform = `translate3d(${scrollTotal}px, 0, 0)`;
-    scrollerIndicator.style.transform = `translate3d(${(scrollTotal / scrollerNavWidthPercent) - scrollerIndicator.offsetWidth}px, 0, 0)`;
+    scrollerIndicator.style.transform = `translate3d(${scrollerIndicatorPosition}px, 0, 0)`;
   }
   if (scrollTotal >= 0) {
     scrollTotal = 0;
     contentScroller.style.transform = `translate3d(${scrollTotal}px, 0, 0)`;
+  }
+  if (scrollerIndicatorPosition <= 0) {
+    scrollerIndicatorPosition = 0;
+    scrollerIndicator.style.transform = `translate3d(${scrollerIndicatorPosition}px, 0, 0)`;
   }
 };
 
